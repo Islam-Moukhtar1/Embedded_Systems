4e74288107a15f9e4b0dce6c64ea9215a4f04146
Islam Moukhtar <islammoukhtar8>
2022-08-23 17:13:32 +0200

    Solved first 3

---
+++ Assignment_6.c
@@ -0,0 +1,65 @@
+/*
+ * main.c
+ *
+ *  Created on: Aug 23, 2022
+ *      Author: islam
+ */
+
+/*
+//problem 1
+#include<stdio.h>
+int main(void)
+{
+	int m=29,*ab;
+	printf("Address of m  : %p\n" ,&m);
+	printf("Value of m  : %d\n" ,m);
+	ab=&m;
+    printf("Now ab is assigned with address of m.");
+	printf("Address of pointer ab  : %p\n" ,ab);
+	printf("Value of pointer ab  : %d\n" ,*ab);
+	m=34;
+	printf("The value of m assigned to 34 now.");
+	printf("Address of pointer ab  : %p\n" ,ab);
+	printf("Value of pointer ab  : %d\n" ,*ab);
+	*ab=7;
+	printf("Address of m  : %p\n" ,&m);
+	printf("Value of m  : %d\n" ,m);
+	return 0;
+}
+*/
+
+/*
+//problem 2
+#include<stdio.h>
+int main(void)
+{
+	char *ptr='\0',characters='A',i;
+	ptr=&characters;
+	do
+	{
+		printf("%c ",*ptr+i);
+		i++;
+	}while(*ptr+i<='Z');
+
+	return 0;
+}
+*/
+
+
+#include<stdio.h>
+#include<string.h>
+int main(void)
+{
+	char str[100],*ptr,i;
+	printf("Input a string : ");
+	fflush(stdin);fflush(stdout);
+	scanf("%s",str);
+	ptr=&str[0]+strlen(str)-1;
+	printf("Reverse of the string is : ");
+	for(i=0;i<strlen(str);i++)
+	{
+		printf("%c",*ptr);
+		ptr--;
+	}printf("\n");
+	return 0;
+}
